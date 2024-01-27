025bb8908fce49303b84ce29ec8afe0b127a23dd
Daniel Whiteman <danielwhiteman@Daniels-MacBook-Air.local>
2024-01-27 12:06:56 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+
+
+def get_exchange_rates(target_currency, app_id):
+    url = f"https://openexchangerates.org/api/latest.json?app_id={app_id}"
+    response = requests.get(url, params={'app_id': app_id})
+
+    if response.status_code != 200:
+        raise Exception(f"Failed API request: {response.status_code}")
+
+    data = response.json()
+    rate = data['rates'][target_currency]
+    return rate
+
+
+def convert_currency(amount, target_currency, app_id):
+    rate = get_exchange_rates(target_currency, app_id)
+    converted_amount = amount * rate
+    return round(converted_amount, 2)
+
+
+def initiate_conversion():
+    app_id = '<insert_api_key>'
+    amount = float(input("Enter the amount you want to exchange: "))
+    target_currency = input("Enter the target currency: ").upper()
+
+    try:
+        converted_amount = convert_currency(amount, target_currency, app_id)
+        print(f"The {amount} in {target_currency} is converted to {converted_amount}")
+    except Exception as e:
+        print(f"There was an error: {e}")
+
+
+initiate_conversion()
